c78ca6d67da8756b7aee0be5826e58f6b5166175
bernie169 <tsbernberg@gmail.com>
2026-04-14 21:03:08 +0200

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -2,8 +2,6 @@
 // Receives conversion postbacks from Easybet (and other brands)
 // Deduplicates, stores to Supabase, and fires back to Mondiad
 
-export const config = { runtime: 'edge' };
-
 const SUPABASE_URL = 'https://gpzovlgzuloevxvutenv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwem92bGd6dWxvZXZ4dnV0ZW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NDE1NDAsImV4cCI6MjA5MDUxNzU0MH0.QJrn3arLWcnY4ACTFpGtbD9pRTIHcMmqr6w8S2OqPdE';
 
